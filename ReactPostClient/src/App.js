590e93f7a47640de84f9128fcd3a0faaf80b47c0
marcio pereira de sousa <114409309+mpsousa1978@users.noreply.github.com>
2024-01-02 09:42:31 -0600

    fixed name

--- ReactPostClient/src/App.js
+++ ReactPostClient/src/App.js
@@ -45,7 +45,7 @@ function App() {
         <div className="col d-flex flex-column justify-content-center align-items-center">
           {showingCreateNewPostForm === false && postCurrentBeingUpdated === null && (
             <div>
-              <h1>Asp.net core React</h1>
+              <h1>Asp.net core React Post</h1>
               <div className="mt-5">
                 <button onClick={GetPosts} className="btn btn-dark btn-lg w-100">
                   Get Post from server
